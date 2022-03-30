5cee4071600a3c2f80a7790d6628253cff056b83
Ivan <adamovskij00@mail.ru>
2022-03-30 13:09:19 +0300

    add App title and p.s

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import GameController from "./components/GameController";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        2048 the game
+      </h1>
+      <GameController/>
+      <p>
+        P.S. Use keyboard arrows for control
+      </p>
     </div>
   );
 }
-
-export default App;
